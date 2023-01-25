557c06dbb806306edfd4ddd4907674a209549d65
[Anthony] <stine.anthony@yahoo.com>
2023-01-25 18:40:59 -0500

    updated status

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const { User, users } = require("./models/user.model");
 const { Company, companies } = require("./models/company.model");
 
 app.get("/api/users/new", (req, res) => {
-  res.json(users);
+  res.status(200).json(users);
 });
 
 app.get("/api/companies/new", (req, res) => {
-  res.json(companies);
+  res.status(200).json(companies);
 });
 
 app.post("/api/user/company", (req, res) => {
-  const newArr = [];
-  const newUser = new User();
-  newArr.push(newUser);
-  const newCompany = new Company();
-  newArr.push(newCompany);
-  res.status(200).json(newArr);
+  // const newArr = [];
+  // const newUser = new User();
+  // newArr.push(newUser);
+  // const newCompany = new Company();
+  // newArr.push(newCompany);
+  res.status(200).json({User: new User(), Company: new Company()});
 });
 
 const PORT = 5001;
